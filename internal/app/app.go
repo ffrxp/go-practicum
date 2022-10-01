19fd1285f17d7ca13ef5e24665e030ce66d19f59
ffrxp <ldm.pubmail@yandex.ru>
2022-10-01 22:38:02 +0300

    sprint 4 fixed (#8)

--- internal/app/app.go
+++ internal/app/app.go
@@ -14,6 +14,9 @@ type ShortenerApp struct {
 	DatabasePath string
 }
 
+var ErrURLDeleted = errors.New("app: URL deleted")
+var ErrCantFindURL = errors.New("app: cannot find URL")
+
 // CreateShortURL creates short URL and return it in full version
 func (sa *ShortenerApp) CreateShortURL(url string, userID int) (string, error) {
 	shortURL := sa.makeShortURL(url)
@@ -43,26 +46,45 @@ func (sa *ShortenerApp) CreateShortURLs(urls []string, userID int) ([]string, er
 }
 
 func (sa *ShortenerApp) GetOrigURL(shortURL string) (string, error) {
-	origURL, err := sa.Storage.GetItem(shortURL)
+	itemRes, err := sa.Storage.GetItem(shortURL)
 	if err != nil {
-		if err.Error() == "not found" {
-			return "", errors.New("cannot find full URL for this short URL")
+		if errors.Is(err, storage.ErrEmptyResult) {
+			return "", ErrCantFindURL
 		}
 		return "", err
 	}
-	return origURL, nil
+	if itemRes.HaveDeletedFlag {
+		return "", ErrURLDeleted
+	}
+	return itemRes.Item, nil
 }
 
 func (sa *ShortenerApp) GetExistShortURL(origURL string) (string, error) {
-	shortURL, err := sa.Storage.GetItemByID(origURL)
+	itemRes, err := sa.Storage.GetItemByID(origURL)
 	if err != nil {
-		if err.Error() == "not found" {
-			return "", errors.New("cannot find short URL for this full URL")
+		if errors.Is(err, storage.ErrEmptyResult) {
+			return "", ErrCantFindURL
 		}
 		return "", err
 	}
-	outputFullShortURL := fmt.Sprintf("%s/%s", sa.BaseAddress, shortURL)
+	if itemRes.HaveDeletedFlag {
+		return "", ErrURLDeleted
+	}
+	outputFullShortURL := fmt.Sprintf("%s/%s", sa.BaseAddress, itemRes.Item)
 	return outputFullShortURL, nil
+}
+
+func (sa *ShortenerApp) ShortURLExist(shortURL string) (bool, error) {
+	// До конца не уверен, что использовать error в рамках штатной работы алгоритма является хорошей идеей,
+	// но пока не успеваю обдумать другие варианты
+	_, err := sa.Storage.GetItem(shortURL)
+	if err != nil {
+		if errors.Is(err, storage.ErrEmptyResult) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
 }
 
 func (sa *ShortenerApp) GetHistoryURLsForUser(userID int) ([]byte, error) {
@@ -81,9 +103,37 @@ func (sa *ShortenerApp) GetHistoryURLsForUser(userID int) ([]byte, error) {
 	return historyByJSON, nil
 }
 
+func (sa *ShortenerApp) UserHaveURLinHistory(userID int, URL string) (bool, error) {
+	history, err := sa.Storage.GetUserHistory(userID)
+	if err != nil {
+		if errors.Is(err, storage.ErrEmptyResult) {
+			return false, nil
+		}
+		return false, err
+	}
+	for i := 0; i < len(history); i++ {
+		if history[i].ShortURL == URL {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
 func (sa *ShortenerApp) UserHaveHistoryURLs(userID int) (bool, error) {
 	history, err := sa.Storage.GetUserHistory(userID)
-	return len(history) != 0, err
+	if err != nil {
+		if errors.Is(err, storage.ErrEmptyResult) {
+			return false, nil
+		}
+		return len(history) != 0, err
+	}
+
+	return true, nil
+}
+
+func (sa *ShortenerApp) MarkDeleteBatchURLs(urls []string) error {
+	err := sa.Storage.MarkDeleteBatchItems(urls)
+	return err
 }
 
 func (sa *ShortenerApp) makeShortURL(url string) string {
